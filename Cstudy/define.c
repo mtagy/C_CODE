925aee081ca1c78143cfe4883feb81b0c17d6257
mtagy <2930668606@qq.com>
2023-05-28 22:11:35 +0800

    C study

---
+++ Cstudy/define.c
@@ -0,0 +1,13 @@
+//#include <stdio.h>
+#define MAX 100
+#define STR "abcdef"
+int main()
+{
+    printf("%d\n", MAX);
+    int a = MAX;
+    printf("%d\n", a);
+    printf("%s\n", STR);
+    // SMAX = 20; // err
+
+    return 0;
+}
---
+++ Cstudy/meiju.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("%d\n", MAX);
+    int a = MAX;
+    printf("%d\n", a);
+    return 0;
+}
