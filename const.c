09e5ad7d6e0f23a68fd5bff63e0e8d863d08629d
mtagy <2930668606@qq.com>
2023-06-09 14:51:42 +0800

    1

---
+++ const.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+// const 修饰的常变量
+int main()
+{
+    const int n = 10;
+    int arr[n] = {0};
+    return 0;
+}
---
+++ meiju.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+//枚举常量
+enum Color
+{
+    //枚举常量
+    RED,
+    GREEN,
+    BLUE
+};
+//性别
+enum Sex
+{
+    //枚举常量
+    MALE,
+    FEMALE,
+    SECRET
+};
+
+int main()
+{
+    //三原色
+    // Red Green Blue
+    int num = 10;
+    enum Color c = RED;
+    RED = 20;
+    MALE = 10;
+    return 0;
+}
