9b7b1dd401cb8ff00de013211747a27f8895375a
mtagy <2930668606@qq.com>
2023-08-11 08:21:03 +0800

    C_study

--- PointerAdvanced/PointerAdvanced/char.c
+++ PointerAdvanced/PointerAdvanced/char.c
@@ -172,7 +172,7 @@ int main()
 	return 0;
 }*/
 
-#include<stdio.h>
+/*#include<stdio.h>
 
 void menu()
 {
@@ -230,18 +230,18 @@ int main()
 			calc(Add);
 			break;
 		case 2:
-			/*ret = Sub(x, y);
-			printf("%d\n", ret);*/
+			//ret = Sub(x, y);
+			//printf("%d\n", ret);
 			calc(Sub);
 			break;
 		case 3:
-			/*ret = Mul(x, y);
-			printf("%d\n", ret);*/
+			//ret = Mul(x, y);
+			//printf("%d\n", ret);
 			calc(Mul);
 			break;
 		case 4:
-			/*ret = Div(x, y);
-			printf("%d\n", ret);*/
+			//ret = Div(x, y);
+			//printf("%d\n", ret);
 			calc(Div);
 			break;
 		case 0:
@@ -254,7 +254,7 @@ int main()
 	} while (input);
 
 	return 0;
-}
+}*/
 //问题：输入0/选择错误仍需要输入操作数
 //改进1：case 1:~case 2:加入输入操作数的代码      
 //缺点：代码冗余 重复度高
@@ -262,3 +262,378 @@ int main()
 // 回调函数：通过函数指针在适当的时候回头调用他所指向的函数
 //calc(Add); calc(Sub); calc(Mul); calc(Div); 
 //函数指针在大型工程中应用多，是C语言中高级语法
+
+/*#include <stdio.h>
+
+int Add(int x, int y)
+{
+	return x + y;
+}
+
+int Sub(int x, int y)
+{
+	return x - y;
+}
+
+int Mul(int x, int y)
+{
+	return x * y;
+}
+
+int Div(int x, int y)
+{
+	return x / y;
+}
+
+int main()
+{
+	int (*pf)(int, int) = Add;
+	//pf是函数指针
+	int (*arr[4])(int, int) = { Add,Sub,Mul,Div };//4个函数类型和函数地址类型相同
+	//arr函数指针数组 ：多个参数相同，返回类型相同的函数的地址放到一个数组中
+	//arr先与[]结合 arr[4]  4个元素的数组
+	//去除数组名和[4] 剩下就是每个元素的类型
+	//int (*)(int, int)  函数指针类型
+	int i = 0;
+	//调用函数
+	for (i = 0; i < 4; i++)
+	{
+		int ret = arr[i](8, 4);
+		printf("%d\n", ret);
+	}
+	return 0;
+}*/
+
+/*#include<stdio.h>
+
+void menu()
+{
+	printf("*******************************************\n");
+	printf("*************1. add       2. sub **********\n");
+	printf("*************3. mul       4. div **********\n");
+	printf("*************0. exit             **********\n");
+	printf("*******************************************\n");
+}
+
+void calc(int (*pf)(int, int))
+{
+
+}
+
+int Add(int x, int y)
+{
+	return x + y;
+}
+
+int Sub(int x, int y)
+{
+	return x - y;
+}
+
+int Mul(int x, int y)
+{
+	return x * y;
+}
+
+int Div(int x, int y)
+{
+	return x / y;
+}
+
+int main()
+{
+	int input = 0;
+	int x = 0, y = 0, ret = 0;
+	int(*pfarr[ ])(int, int) = { 0,Add,Sub,Mul,Div };
+	do
+	{
+		menu();
+		printf("请选择：>");
+		scanf("%d", &input);
+		if (input == 0)
+		{
+			printf("退出计算器\n");
+		}
+		else if (input >= 1 && input <= 4)//简化代码 如果增加新功能，不需要再写多个case语句避免冗余
+		{
+			printf("请输入两个操作数：>");
+			scanf("%d %d", &x, &y);
+			ret = pfarr[input](x, y);
+			printf("%d\n", ret);
+		}
+		else
+		{
+			printf("选择错误，请重新输入\n");
+		}
+	} while (input);
+	return 0;
+}*/
+
+/*#include<stdio.h>
+
+int Add(int x, int y)
+{
+	return x + y;
+}
+
+int Sub(int x, int y)
+{
+	return x - y;
+}
+
+int Mul(int x, int y)
+{
+	return x * y;
+}
+
+int Div(int x, int y)
+{
+	return x / y;
+}
+
+int main()
+{
+	int(*pfarr[])(int, int) = { 0,Add,Sub,Mul,Div };//函数指针数组
+	int(*(*ppfarr)[5])(int, int)= &pfarr;//数组的地址放到一个数组指针中去  指向函数指针数组的指针
+	//ppfarr先与*结合--->说明ppfarr是指针
+	//(*ppfarr)[5]--->说明ppfarr指向了数组，数组有5个元素
+	//int(*)(int, int)--->说明ppfarr指向的数组的每个元素的类型为函数指针
+
+	//思路：函数指针--->函数指针数组--->指向函数指针的数组
+	return 0;
+}*/
+
+//#include <stdio.h>
+//#include< stdlib.h >
+//#include <search.h>
+//void bubble(int arr[], int sz)
+//{
+//	//冒泡排序的趟数 sz-1
+//	int i = 0, j = 0;
+//	for (i = 0; i < sz - 1; i++)
+//	{
+//		int flag = 1;//假设数组是排好序的
+//		for (j = 0; j < sz - 1 - i; j++)//每趟比较次数：sz-1-i
+//		{//每趟的比较过程  排成升序     
+//
+//		//不同类型（非int）的数据排序：要改变的是相邻两个元素的比较
+//		// eg:排序结构体类型数据：一组人的信息  
+//		//不同类型数据的比较方式不一样，不是所有的数据都可以用>比较
+//		//类比calc()   排序不同类型数据的比较方式不一样，将比较的功能封装成一个函数，实现不同类型数据的比较
+//		//同理qsort() 中的参数 cmp实现的就是比较的功能：比较函数的地址 
+//			if (arr[j] > arr[j + 1])
+//			{
+//				int tmp = arr[j];
+//				arr[j] = arr[j + 1];
+//				arr[j + 1] = tmp;
+//				flag = 0;
+//			}
+//		}
+//		if (flag == 1) break;//提高效率
+//	}
+//}
+//
+////比较两个整型的函数：
+////e1指向一个整数 e2指向另外一个整数
+//int cmp_int(const void* e1, const void* e2)
+//{//void* 类型的指针不能直接进行解引用操作  if(*e1>*e2)  错误
+// //void*
+//	/*int a = 10;
+//	char* pc = &a;//会报warning &a是int*  类型不匹配
+//	void* pv = &a;//没有warning  */
+//	//void*:无具体类型的指针 可以接收任意类型的指针，但是不能进行解引用操作和+-整数操作(都需要具体的类型，而void*没有明确的类型)
+//	//不知道传过来的函数地址是什么类型的，所以用void*接收
+//
+//	/*
+//	if(_*(int*)e1>*(int*)e2)  return 1;
+//	else if(_*(int*)e1==*(int*)e2)  return 0;
+//	else return -1;
+//	*/
+//
+//	//return *(int*)e1 - *(int*)e2;
+//	//升序简洁写法  qsort默认排升序 想要排降序，与默认返回值的逻辑相反即可
+//	return *(int*)e2 - *(int*)e1;//降序
+//
+//}
+//int main()
+//{
+//	//int arr[] = { 9,8,7,6,5,4,3,2,1,0 };//排升序
+//	int arr[] = { 0,1,2,3,4,5,6,7,8,9 };//排降序
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	//bubble(arr, sz);
+//	qsort(arr, sz, sizeof(arr[0]), cmp_int);//函数名即地址
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//	return 0;
+//}
+//冒泡排序核心思想：两两相邻的元素进行比较
+//每一趟冒泡排序排好一个数据
+//n个数据需要n-1趟冒泡排序
+//第一趟冒泡排序中比较n个元素需要比较n-1次(n-1个相邻的对数）
+//第二趟冒泡排序中比较n-1个元素需要比较n-2次
+
+//问题：这个冒泡排序在0123456789已经是升序时还会继续两两比较(不会交换，但是会比较)
+//优化：flag==1 排好序 每趟比较完判断flag是否置0(即是否有元素交换)，当没有元素进行交换，即不必进行下一趟排序
+//不好的点：这个bubble()形参是int arr[] 只能进行整型数据的排序    
+//解决：库函数qsort() ： 使用快速排序的思想实现的一个排序函数
+//void qsort( void *base, size_t num, size_t width, int (__cdecl *compare )(const void *elem1, const void *elem2 ) );
+//__cdecl 函数调用约定（约定函数如何调用，按照c的方式去调用）（使用时可以不写）
+// 简化
+//qsort() 可以排序任意类型的数据  
+/*void qsort( void *base, //你要排序的数据的起始位置
+				  size_t num, //待排序数据元素的个数
+				  size_t width,//待排序数据元素的大小（单位：byte）
+				  int (*cmp )(const void *e1, const void *e2 ) );//  函数指针  比较函数的地址*/
+				  //cmp是比较函数的地址，e1指向了要比较的第一个元素，e2指向了要比较的第二个元素，e1,e2是要比较的两个元素的地址
+				  //cmp主动调用指向的比较函数，将e1，e2指向的两个元素进行比较   
+				  //不管排序什么类型的数据 都能根据你提供不同的比较函数进行数据比较 
+/*#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int cmp_int(const void* e1, const void* e2)
+{
+	return *(int*)e2 - *(int*)e1;//降序
+}
+
+void test1()//qsort排序整型数据
+{
+	int arr[] = { 0,1,2,3,4,5,6,7,8,9 };//排降序
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	qsort(arr, sz, sizeof(arr[0]), cmp_int);//函数名即地址
+	int i = 0;
+	for (i = 0; i < sz; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+}
+
+struct Stu
+{
+	char name[20];
+	int age;
+};
+
+int cmp_stu_name(const void* e1, const void* e2)
+{
+	return strcmp(((struct Stu*)e1)->name, ((struct Stu*)e2)->name);
+	//字符串比较大小  strcmp()
+	//strcmp()的返回值是 >0 =0 <0(e1<e2)
+	//strcmp()比较时是一对一对的进行比较,相等就找下一对
+	//eg  abcdef  abbqwrttt  a=a b=b c>b 所以abcdef> abbqwrttt
+}
+
+int cmp_stu_age(const void* e1, const void* e2)
+{
+	return ((struct Stu*)e1)->age - ((struct Stu*)e2)->age;
+}
+
+void test2()//使用qsort排序结构体数据
+{
+	struct Stu s[] = { {"zhangsan",15},{"lisi",30},{"wangwu",25} };
+	int sz = sizeof(s) / sizeof(s[0]);
+	qsort(s, sz, sizeof(s[0]), cmp_stu_name);
+	//qsort(s, sz, sizeof(s[0]), cmp_stu_age);
+
+}
+
+int main()
+{
+	//test1();
+	test2();
+	return 0;
+}*/
+
+
+/*#include <stdio.h>
+#include <string.h>
+
+int cmp_int(const void* e1, const void* e2)
+{
+	return *(int*)e1 - *(int*)e2;//升序
+}
+
+struct Stu
+{
+	char name[20];
+	int age;
+};
+
+int cmp_stu_name(const void* e1, const void* e2)
+{
+	return strcmp(((struct Stu*)e1)->name, ((struct Stu*)e2)->name);
+}
+
+int cmp_stu_age(const void* e1, const void* e2)
+{
+	return ((struct Stu*)e1)->age - ((struct Stu*)e2)->age;
+}
+
+void Swap(char* buf1, char* buf2, int width)
+{//每个字节对进行交换
+	int i = 0;
+	for (i = 0; i < width - 1; i++)
+	{
+		char tmp = *buf1;
+		*buf1 = *buf2;
+		*buf2 = tmp;
+		buf1++;
+		buf2++;
+	}
+}
+
+void bubble(void* base, int num, int width, int (*cmp)(const void* e1, const void* e2))
+{
+	int i = 0, j = 0, flag = 1;
+	for (i = 0; i < num - 1; i++)
+	{
+		for (j = 0; j < num - 1 - i; j++)
+		{
+			if (cmp((char*)base + j * width, (char*)base + (j + 1) * width) > 0)//起始地址+偏移量
+			{
+				Swap((char*)base + j * width, (char*)base + (j + 1) * width, width);
+				flag = 0;
+			}
+		}
+		if (flag == 1) break;
+	}
+}
+
+void test1()
+{
+	int arr[] = { 9,8,7,6,5,4,3,2,1,0 };//排升序
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	bubble(arr, sz, sizeof(arr[0]), cmp_int);//函数名即地址
+	int i = 0;
+	for (i = 0; i < sz; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+}
+
+void test2()
+{
+	struct Stu s[] = { {"zhangsan",15},{"lisi",30},{"wangwu",25} };
+	int sz = sizeof(s) / sizeof(s[0]);
+	//bubble(s, sz, sizeof(s[0]), cmp_stu_name);
+	bubble(s, sz, sizeof(s[0]), cmp_stu_age);
+}
+
+int main()
+{
+	//test1();
+	test2();
+	return 0;
+}*/
+
+
+#include<stdio.h>
+
+int main()
+{
+
+	return 0;
+}
